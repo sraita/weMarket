ea7a1c1892274cb8e5a925c2f1c6462247fe9a84
sraita <sraita@139.com>
2017-09-02 03:32:21 +0800

    select product info popup

--- weMarketWeb/client/product/product.js
+++ weMarketWeb/client/product/product.js
@@ -1,3 +1,21 @@
+changePrice = function (type) {
+  var qty = Number($('#goods_number').val()) || 1;
+  //var qty = 0;
+
+  if (type == 1) {
+    qty--;
+  }
+  if (type == 3) {
+    qty++;
+  }
+  if (qty <= 0) {
+    qty = 1;
+  }
+  if (!/^[0-9]*$/.test(qty)) {
+    qty = 1;
+  }
+  $('#goods_number').val(qty);
+}
 Template.product.onRendered(function(){
   var swiper = new Swiper('.swiper-container', {
       pagination: '.swiper-pagination',
@@ -10,6 +28,13 @@ Template.product.onRendered(function(){
   });
 });
 
-Template.product.events({
+Template.product.helpers({
 
+});
+
+Template.product.events({
+  // 添加到购物车
+  'click .addToCart': function(){
+    $("#addToCart").popup();
+  }
 });
