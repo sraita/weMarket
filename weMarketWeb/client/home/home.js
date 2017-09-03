8c3501d0f4703d1690b64e9a8e4bc0829cd05b56
sraita <sraita@139.com>
2017-09-04 01:53:13 +0800

    home swiper ui

--- weMarketWeb/client/home/home.js
+++ weMarketWeb/client/home/home.js
@@ -1,5 +1,10 @@
 Template.home.onRendered(function(){
   Meteor.subscribe('products_by_seller','RTsZ64Cc8iyoc4BmW', 100);
+  var swiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+      autoplay : 5000,
+      loop: true
+  });
 });
 
 Template.home.helpers({
