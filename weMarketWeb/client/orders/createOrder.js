3319cabbfef716cf9b42bc86be8ab340d61f8c04
sraita <sraita@139.com>
2017-09-09 18:39:48 +0800

    创建订单时， 用户可以选择收货地址

--- weMarketWeb/client/orders/createOrder.js
+++ weMarketWeb/client/orders/createOrder.js
@@ -1,3 +1,10 @@
+Template.createOrder.onRendered(function(){
+  Meteor.subscribe('contact_list', function(){
+    var contact = Contact.findOne({user_id: Meteor.userId()},{sort:{createdAt:-1}});
+    Session.set('selectedAddr', contact);
+  });
+});
+
 Template.createOrder.helpers({
   shoppings: function(){
     return Shopping.find({}).fetch();
@@ -11,10 +18,32 @@ Template.createOrder.helpers({
       price += item.product_num * item.product_price;
     });
     return price.toFixed(2);
+  },
+  contact: function(){
+    return Session.get('selectedAddr');
+  },
+  contacts: function(){
+    return Contact.find({user_id: Meteor.userId()},{sort:{createdAt:-1}}).fetch();
+  },
+  selectedAddr: function(_id){
+    if(Session.get('selectedAddr') && Session.get('selectedAddr')._id === _id){
+      return true;
+    } 
+    return false;
   }
 });
 
 Template.createOrder.events({
+  'click #user_addr': function(e){
+    return $('#selectAddr').popup();
+  },
+  'click .selectAddrItem': function(e){
+    // console.log(this);
+    Session.set('selectedAddr',this);
+  },
+  'click .selectAddrBtn': function(e){
+    return $.closePopup();
+  },
   'click #createOrder': function(e){
     Meteor.call('getOrderNumber', function(err, result){
       if(err){
@@ -23,13 +52,13 @@ Template.createOrder.events({
       }
       var order_no = result;
       var user = Meteor.user();
-
+      var addr = Session.get('selectedAddr');
       var obj = {
         user_id: user._id,
         user_name: (user.profile && user.profile.name)? user.profile.name : user.username,
         user_icon: (user.profile && user.profile.icon)? user.profile.icon:'/img/userPicture.png',
 
-        user_addr:$('#user_addr option:selected').text(),
+        user_addr: addr,
 
         seller_id: Router.current().params.query.s || 'RTsZ64Cc8iyoc4BmW',
         products: [],
