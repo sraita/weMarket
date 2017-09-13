69d630ab1a52392f19e291f557caddce84f313e2
sraita <sraita@139.com>
2017-09-13 23:30:20 +0800

    fix oauth callback error

--- weMarketWeb/client/router_client.js
+++ weMarketWeb/client/router_client.js
@@ -3,7 +3,7 @@ var app_secret = 'afd33592eb230c2f8f9936881b2383ba';
 var scope = 'snsapi_userinfo';
 var state = Date.now();
 
-var redirect_uri = encodeURIComponent('market.raidcdn.cn/oauth/wexhat');
+var redirect_uri = encodeURIComponent('market.raidcdn.cn/oauth/wechat');
 var auth_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+
                '&redirect_uri='+redirect_uri+
                '&response_type=code&scope='+scope+
