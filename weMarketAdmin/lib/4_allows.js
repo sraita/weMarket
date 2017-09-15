d4a438bfb5805fc20cbc180bc111365fa6cca36c
sraita <sraita@139.com>
2017-09-15 12:28:48 +0800

    完善： 订单详情页

--- weMarketAdmin/lib/4_allows.js
+++ weMarketAdmin/lib/4_allows.js
@@ -41,7 +41,7 @@ Orders.allow({
   },
   update: function (userId, doc, fields, modifier) {
     // can only change your own documents
-    return doc.owner === userId;
+    return true;
   },
   remove: function (userId, doc) {
     // can only remove your own documents
---
+++ weMarketWeb/client/orders/orderInfo.js
@@ -0,0 +1,25 @@
+Template.orderInfo.helpers({
+  getProductTotalPrice: function(){
+    return (this.product_num * this.product_price).toFixed(2);
+  }
+});
+
+Template.orderInfo.events({
+  'click #cancelOrder': function(e){
+    console.log(this._id);
+    Orders.update({_id: this._id},{
+      $set:{
+        status: 0
+      }
+    },function(err, result){
+      if(err){
+        console.log(err);
+        return $.toast('请重试','cancel');
+      }
+      $.toast('已取消');
+      Meteor.setTimeout(function(){
+        PUB.back()
+      },1000);
+    })
+  }
+})
